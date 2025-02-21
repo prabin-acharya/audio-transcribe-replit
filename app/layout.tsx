d53606e8a8b0616fb321ee5adab089bfbe1b33d9
Prabin Acharya <acharyaprabin101@gmail.com>
2025-02-22 01:16:14 +0545

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Transcribe Audio",
-  description: "Built by Prabin",
+  description: "",
 };
 
 export default function RootLayout({
